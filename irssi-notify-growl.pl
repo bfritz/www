0c2eca06cc98fafedb54ebdc7af1021eba454841
Allan Willems Joergensen <allan@nowhere.dk>
2010-03-06 20:30:02 +0100

    Added comments, removed debug

--- irssi-notify-growl.pl
+++ irssi-notify-growl.pl
@@ -7,7 +7,6 @@ use warnings;
 use IPC::Message::Minivan;
 use Encode;
 use Growl::GNTP;
-use Data::Dumper;
 
 
 if ($#ARGV != 0) {
@@ -17,9 +16,11 @@ if ($#ARGV != 0) {
 
 my $port = $ARGV[0];
 
+# Connect to the minivan and subscribe to the #irssi channel
 my $van = IPC::Message::Minivan->new(host => 'localhost');
 $van->subscribe("#irssi");
 
+# Setup the Growl object
 my $growl = Growl::GNTP->new(
         AppName => "Irssi",
         PeerHost => "localhost",
@@ -28,10 +29,12 @@ my $growl = Growl::GNTP->new(
         AppIcon => "http://dl.dropbox.com/u/262048/www.nowhere.dk/files/irc.png"
 );
 
+# Register our application
 $growl->register([
         { Name => "irssi", },
 ]);
 
+# In case the connection is up, notify the user that we are up and running.
 $growl->notify(
                 Event => "irssi",
                 Title => "Minivan",
@@ -40,13 +43,18 @@ $growl->notify(
 
         
 while (1) {
+	# Ask minivan for new messages, wait 5 seconds
+	# Restart the loop if we didn't get data
 	if (my $cmd = $van->get(5,[])) {
+		# Is the channel #irssi
 		if ($cmd->[0] eq '#irssi') {
 			my $c=$cmd->[1];
 
+			# Read out the message and summary/headline for the notification
 			my $message = $c->{msg};
 			my $summary = $c->{summary};
                         
+			# Notify Growl
                         $growl->notify(
                                         Event => "irssi",
                                         Title => $summary,
